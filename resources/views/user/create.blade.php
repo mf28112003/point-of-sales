b4ebd19bc6d0d7c21fadf6b697febe8cc674d661
mf28112003 <furqonmuhammad927@gmail.com>
2023-10-31 20:05:23 +0700

    interface

---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,49 @@
+<x-app>
+    <div class="card">
+        <div class="card-header">
+            <div class="card-title">
+                Tambah Data user
+            </div>
+        </div>
+        <div class="card-body">
+            <form method="post" action="{{ url('user') }}">
+                @csrf
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="" class="control-label">Username</label>
+                            <input type="text" name="username" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="" class="control-label">Nama</label>
+                            <input type="text" name="nama" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="" class="control-label">Email</label>
+                            <input type="email" name="email" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="" class="control-label">Password</label>
+                            <input type="password" name="password" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <button class="btn btn-primary float-right">
+                            <i class="fas fa-save"></i>Simpan
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</x-app>
