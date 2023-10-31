13072fdf82e4a24f2a920e804c167de087508d26
mf28112003 <furqonmuhammad927@gmail.com>
2023-10-31 15:23:13 +0700

    commit in branch furqon

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,10 +2,10 @@
     <div class="card">
         <div class="card-header">
             <div class="card-title">
-                Data satuan
+                Data User
             </div>
-            <a href="{{url('satuan/create')}}" class="btn btn-primary float-right">
-                <i class="fas fa-plus">Tambah satuan</i>
+            <a href="{{ url('user/create') }}" class="btn btn-primary float-right">
+                <i class="fas fa-plus"></i>Tambah User
             </a>
         </div>
         <div class="card-body">
@@ -13,31 +13,30 @@
                 <thead>
                     <th width="100px">No</th>
                     <th width="150px">Aksi</th>
+                    <th>Username</th>
                     <th>Nama</th>
-                    <th>harga jual</th>
+                    <th>Email</th>
                 </thead>
                 <tbody>
-                    @foreach($list_satuan as $satuan)
-                    <tr>
-                        <td>{{$loop->iteration}}</td>
-                        <td>
-                            <div class="btn-group">
-                                <a href="{{url ('satuan', $satuan->id)}}" class="btn btn-info">
-                                    <i class="fas fa-info"></i>
-                                </a>
-                                <a href="{{url ('satuan', $satuan->id)}}/edit" class="btn btn-warning">
-                                    <i class="fas fa-edit"></i>
-                                </a>
-                                <x-button.delete id="{{$satuan->id}}"/>
-                            </div>
-                        </td>
-                        <td>
-                            {{$satuan->nama}}
-                        </td>
-                        <td>
-                            Rp. {{number_format ($satuan->harga_jual)}}
-                        </td>
-                    </tr>
+                    @foreach ($list_user as $user)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>
+                                <div class="btn-group">
+                                    <a href="{{ url('user', $user->id) }}" class="btn btn-info">
+                                        <i class="fas fa-info"></i>
+                                    </a>
+                                    <a href="{{ url('user', $user->id) }}/edit" class="btn btn-warning">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <x-button.delete id="{{ $user->id }}" />
+
+                                </div>
+                            </td>
+                            <td> {{ $user->username }}</td>
+                            <td> {{ $user->nama }}</td>
+                            <td> {{ $user->email }}</td>
+                        </tr>
                     @endforeach
                 </tbody>
             </table>
