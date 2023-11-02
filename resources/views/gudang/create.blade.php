9e3a2c07eb9f5cdab7bf42bcd21e57ab45aa2ad7
rezaidulputra <ezaiidulputra@gmail.com>
2023-11-02 13:47:25 +0700

    gudang

--- resources/views/gudang/create.blade.php
+++ resources/views/gudang/create.blade.php
@@ -2,36 +2,37 @@
     <div class="card">
         <div class="card-header">
             <div class="card-title">
-                Edit
+                Tambah gudang
             </div>
         </div>
         <div class="card-body">
-            <form method="post" action="{{url('barang', $barang->id)}}">
+            <form method="post" action="{{url ('gudang')}}">
                 @csrf
-                @method('put')
                 <div class="row">
                     <div class="col-md-12">
                         <div class="form-group">
-                            <label for="" class="control-label">nama </label>
-                            <input type="text" name="nama" value="{{$barang->nama}}" class="form-control">
+                            <label for="" class="control-label">nama</label>
+                            <input type="text" name="nama" class="form-control">
                         </div>
                     </div>
                     <div class="col-md-12">
                         <div class="form-group">
-                            <label for="" class="control-label">harga</label>
-                            <input type="number" name="harga" value="{{$barang->harga}}" class="form-control">
-                        </div>
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <label for="" class="control-label">foto</label>
-                            <input type="text" name="foto" value="{{$barang->foto}}" class="form-control">
-                        </div>
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <label for="" class="control-label">deskripsi</label>
-                            <input type="text" name="deskripsi" value="{{$barang->deskripsi}}" class="form-control">
+                            <label for="" class="control-label">jumlah barang </label>
+                            <input type="number" name="jumlah_barang" class="form-control">
                         </div>
                     </div>
+                    {{-- <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="" class="control-label">deskripsi </label>
+                            <input type="text" name="deskripsi" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="" class="control-label">foto </label>
+                            <input type="text" name="foto" class="form-control">
+                        </div>
+                    </div> --}}
                     <div class="row">
                         <div class="col-md-12">
                             <button class="btn btn-primary float-right">
